96fb0782acae098524a47421c0038afb8cefa71d
shantik998 <shantik998@gmail.com>
2023-01-22 23:23:53 -0600

    completed project

--- user_math.py
+++ user_math.py
@@ -1,6 +1,9 @@
 """
-Always start with a docstring that describes what the module does.
-Include your name and the date.
+Shanti Kandel
+Data Analytics Module 2 Project 2
+Jan 22, 2023
+
+This project aims to write functions through Math module to perform common mathmatical calculations.
 
 Use built-in functions and 
 functions from the math module.
@@ -20,14 +23,14 @@ def get_area_of_lot(length, width):
     """
     Return area of a lot given the length and width of the lot.
 
-    Could this fail?
+    It could result ValueError if the input value are not numbers or negetive or is 0.
 
     """
 
     # Use a try / except / finally block when something 
     # could go wrong
     try: 
-        area = 0 # fix this
+        area = lenth * width
         return area
     except Exception as ex:
         print(f"Error: {ex}")
@@ -35,6 +38,46 @@ def get_area_of_lot(length, width):
 
 
 # define more functions here (see instuctions)
+
+def defect_product(*args):
+    """
+    returns counts of defected product produced in an hour
+    """
+    defect_count = 0
+    for number in args:
+        if isinstance(number, int) and number >= 0:
+            defect_count += number
+        else:
+            raise ValueError("All inputs must be non-negative integers.")
+    return defect_count
+
+#---------------------------------------------------------------
+
+def sales_of_product(medium, large, small, operating_expenses):
+    """
+    Returns total number of products sold based on their size
+    """
+    try:
+        total_sales = medium + large + small
+        revenue = operating_expenses - total_sales
+        return revenue
+    except Exception as e:
+        print(f'An error occurred: {e}')
+
+
+    #-------------------------------------------------------------
+
+def revenue_after_discount(unit_price, units_sold, discount_percentage):
+    """
+    Calculates the revenue after discount for a product
+    """
+    total_price = unit_price * units_sold
+    discount_amount = (discount_percentage/100) * total_price
+    revenue_after_discount = total_price - discount_amount
+    return revenue_after_discount
+
+
+#------------------------------------------------------------------------
 
 
 
@@ -50,3 +93,15 @@ if __name__ == "__main__":
 
     # call your functions here (see instructions)
     print("your code here")
+    print("Explore some functions in the math module")
+    print()
+    print(f"math.comb(5,1) = {math.comb(5,1)}")
+    print(f"math.perm(5,1) = {math.perm(5,1)}")
+    print(f"get_area_of_lot(6, 2) = {get_area_of_lot(6, 2)}")
+    print()
+    print(f"number of defect product produced per hour ={defect_product(4, 8, 12)}")
+    print()
+    print(f"sales of products based on product size ={sales_of_product(12, 30, 32)}")
+    print()
+    print(f"revenue after deducting discount= {revenue_after_discount(22, 24, 26)}")
+    print()
