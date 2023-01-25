312b74579990a6819f32898b89df3260eb714814
shantik998 <shantik998@gmail.com>
2023-01-24 20:24:38 -0600

    Completed Project

---
+++ useroopout.py
@@ -0,0 +1,49 @@
+ /usr/bin/env /usr/local/bin/python3 /Users/shantikandel/.vscode/extensions/ms-python.python-2023.1.10091012/pythonFiles/lib/python/debugpy/adapter/../../debugpy/launcher 59143 -- /Users/shantikandel/datafun_project2/datafun-02-functions/pybuddy_oopd.py 
+
+The default interactive shell is now zsh.
+To update your account to use zsh, please run `chsh -s /bin/zsh`.
+For more details, please visit https://support.apple.com/kb/HT208050.
+(base) Shantis-MacBook-Pro:datafun-02-functions shantikandel$  /usr/bin/env /usr/local/bin/python3 /Users/shantikandel/.vscode/extensions/ms-python.python-2023.1.10091012/pythonFiles/lib/python/debugpy/adapter/../../debugpy/launcher 59143 -- /Users/shantikandel/datafun_project2/datafun-02-functions/pybuddy_oopd.py 
+
+Welcome, I'm a new PyBuddy.
+
+I'm Alice.
+I'm a Species.CAT with 4 legs.
+I weigh 8.12 kgs.
+I've been alive for 0:00:00.001709.
+I know:
+  - Git
+  - GitHub
+  - Python
+  - Markdown
+  - VS Code
+
+
+You'll need curiousity, the ability to search the web, 
+and the tenacity and resourcefulness
+to solve all kinds of challenges.
+Let's get started! 
+
+        
+
+Welcome, I'm a new PyBuddy.
+
+I'm Rex.
+I'm a Species.DOG with 4 legs.
+I weigh 10.44 kgs.
+I've been alive for 0:00:00.001799.
+I know:
+  - Git
+  - GitHub
+  - Python
+  - Markdown
+  - VS Code
+
+
+You'll need curiousity, the ability to search the web, 
+and the tenacity and resourcefulness
+to solve all kinds of challenges.
+Let's get started! 
+
+        
+(base) Shantis-MacBook-Pro:datafun-02-functions shantikandel$ 
